5903516f91783dd6581165aef5168954c64ac96b
SirSorensen <davidmsoerensen@itu.dk>
2023-12-02 13:41:23 +0100

    Removed prettyfication in textGetter.py

--- Old Modules/textGetter.py
+++ Old Modules/textGetter.py
@@ -6,14 +6,6 @@ spell = str.lower("ACCELERANDO")
 url = f'https://a5e.tools/spell/{spell}'
 data = requests.get(url)
 
-if False:
-    data = BeautifulSoup(data.text, "html.parser")
-    data = re.sub(r'(?:\n[^\S\n]*){3,}', r'\n\n', data.text)
-
-    # dump resulting text to file
-    with open(f"soup_{spell}.txt", "w") as out_f:
-        out_f.write(data)
-else:
-    # dump resulting text to file
-    with open(f"original_{spell}.txt", "w") as out_f:
-        out_f.write(data.text)
+# dump resulting text to file
+with open(f"original_{spell}.txt", "w") as out_f:
+    out_f.write(data.text)
