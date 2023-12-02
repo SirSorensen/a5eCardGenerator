cb15b58afc2e503ab8e31eb8908f3aafe19224b1
SirSorensen <davidmsoerensen@itu.dk>
2023-12-02 13:50:24 +0100

    Better layout of spell.py code

--- Data Types/Spell.py
+++ Data Types/Spell.py
@@ -1,3 +1,10 @@
+class MaterialComponent:
+    def __init__(self, text: str):
+        self.text = text
+        self.consumed = self.evalIfConsumed(text)
+    # TODO: Check if this works
+    def evalIfConsumed(text: str) -> bool:
+        return "consumed" in text.lower()
 
 class Spell:
     def __init__(self,
@@ -16,70 +23,43 @@ class Spell:
         
         # Name = "*"
         self.name = name
-
         # Summary = "*"
         self.summary = summary
-
         # Level = Cantrip/0, 1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 8th, 9th
         self.level = level
-
         # Classical School = Abjuration, Conjuration, Divination, Enchantment, Evocation, Illusion, Necromancy, Transmutation
         self.classical_school = classical_school
-
         # Spell School(s) = Acid, Affliction, Air, Arcane, Attack, Beasts, Chaos, Cold, Communication, Compulsion, Divine, Earth, Enhancement, Evil, Fear, Fire, Force, Good, Healing, Knowledge, Law, Lightning, Movement, Nature, Necrotic, Negation, Obscurement, Planar, Plants, Poison, Prismatic, Protection, Psychic, Radiant, Scrying, Senses, Shadow, Shapechanging, Sound, Storm, Summoning, Technological, Teleportation, Terrain, Thunder, Transformation, Utility, Water, Weaponry, Weather
         self.spell_schools = spell_schools
-
         # Class(es) = Artificer, Bard, Cleric, Druid, Herald, Sorcerer, Warlock, Wizard
         self.classes = classes
-
         # Casting Time = 1 Action, 1 Bonus Action, 1 Hour, 1 Minute, 1 Reaction, 10 Minutes, 12 Hours, 24 Hours, 8 Hours, 1 Week
         self.casting_time = casting_time
-
         # Ritual? = True, False
         self.ritual = ritual
-
         # Range = Self, Touch, Short (30 feet), Medium (60 feet), Long (120 feet),Unlimited, Special
         self.range = range
-
         # Target = "*"
         self.target = target
-
         # Area = "*" (often "<x> feet (radius <area-shape>)?")
         self.area = area
-
         # Area Shape = Cone, Cube, Cylinder, Line, Sphere
         self.area_shape = area_shape
-
         # Components = Material, Seen, Vocalized
         # Material Components = "*"
         self.component_material = MaterialComponent(component_material)
         self.component_seen = component_seen
         self.component_vocalized = component_vocalized
-
         # Duration = Instantaneous, 1 round, 1 minute, 10 minutes, 1 hour, 8 hours, 24 hours, 7 days, 10 days, 30 days, Until dispelled, Until dispelled or triggered, Special, Concentration (1 round), Concentration (1 minute), Concentration (10 minutes), Concentration (1 hour), Concentration (8 hours), Concentration (24 hours), Concentration (7 days), Concentration (10 days), Concentration (30 days), Concentration (Until dispelled), Concentration (Until dispelled or triggered), Concentration (Special)
         self.duration = duration
-
         # Saving Throw = "*"
         self.saving_throw = saving_throw
-
         # Description = "*"
         self.description = description
-
         # Cast at Higher Levels = "*"
         self.cast_at_higher_levels = cast_at_higher_levels
-
         # Rare versions = (title:"*", description:"*")
         self.rare_versions = rare_versions
-
         # Source = "*"
         self.source = source
 
-
-class MaterialComponent:
-    def __init__(self, text: str):
-        self.text = text
-        self.consumed = self.evalIfConsumed(text)
-
-    # TODO: Check if this works
-    def evalIfConsumed(text: str) -> bool:
-        return "consumed" in text
