7460e2004f1646009e4f8968fd524d09fbf35cc1
SirSorensen <davidmsoerensen@itu.dk>
2023-12-02 20:29:49 +0100

    Fix prettify_soup method call

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from Data_Structures.Spell import Spell
 #print("Encoding = " + spell_source_text.encoding)
 
 abstraction_spell = Spell(web_location="abstraction")
-abstraction_spell.prettify_soup()
 
 print(abstraction_spell)
+abstraction_spell.prettify_soup()
