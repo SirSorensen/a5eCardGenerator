8bc37bd18c8a89431297349fbabfdc5df7910434
SirSorensen <davidmsoerensen@gmail.com>
2023-12-10 22:28:31 +0100

    Feat update to match new version

--- data_forge/data_structures/feat.py
+++ data_forge/data_structures/feat.py
@@ -1,13 +1,25 @@
 from data_forge.data_structures.card import Card
 
+field_classes = []
+
+field_ids = []
+
 class Feat(Card):
-    def __init__(self,
-            name: str = "", origin: str = "", summary: str = "", prerequisites: str = "", description: str = ""):
-        self.name = name
-        self.origin = origin
-        self.summary = summary
-        self.prerequisites = prerequisites
-        self.description, self.sub_feats = SubFeat.divideDescription(description)
+    def __init__(self, title : str, source_code : str):
+        # Title = "*"
+        self.title = title
+
+        super(Feat, self).__init__(title, source_code, field_classes, field_ids)
+
+        self.set_fields(self.extract_fields())
+    
+    def set_fields(self, field_dict : dict):
+        return
+        self.name = field_dict.get('name')
+        self.origin = field_dict.get('origin')
+        self.summary = field_dict.get('summary')
+        self.prerequisites = field_dict.get('prerequisites')
+        self.description, self.sub_feats = SubFeat.divideDescription(field_dict.get('description'))
 
 
 class SubFeat:
@@ -25,3 +37,4 @@ def divideDescription(text: str) -> (str, list[SubFeat]):
     
     # split text into body and sub-feats
     return body, sub_feats
+
