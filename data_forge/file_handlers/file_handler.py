0f9c734f95ef0d88b9789cd08e55368474bda067
SirSorensen <davidmsoerensen@gmail.com>
2023-12-11 22:01:10 +0100

    FileHandler: Optimised gen_card_..._directory functions read_file now raises an error if the files being read doesn't exist. Kept move_old_files & rename_old_files

--- data_forge/file_handlers/file_handler.py
+++ data_forge/file_handlers/file_handler.py
@@ -1,6 +1,8 @@
 import os
+import re
 
 global_output_directory = r"Outputs/"
+global_html_output_directory = rf"{global_output_directory}HTML/"
 global_source_code_output_directory = rf"{global_output_directory}Source_Code/"
 global_list_output_directory = rf"{global_source_code_output_directory}Lists/"
 
@@ -12,11 +14,10 @@ class FileHandler:
             with open(abs_filepath, "r", encoding='utf-8') as file:
                 content = file.read()
                 file.close()
+            return content  
+        
         else:
-            print(f"File not found: {abs_filepath}")
-            content = ""
-
-        return content        
+            raise ValueError(f"ERROR! File {abs_filepath} does not exist!")      
 
     # Returns the contents of the written file
     def write_to_file(content:str, output_abs_filepath:str):
@@ -50,25 +51,136 @@ class FileHandler:
 
     def does_file_exist(abs_filepath : str) -> bool:
         return os.path.exists(abs_filepath)
+    
+
+    def __gen_card_code_directory(card_type : str, context_name : str, code_type : str, file_extension : str = ".html"):
+        return rf"{global_html_output_directory}{card_type}s/{code_type}/{context_name}{file_extension}"
+    
+    def __gen_source_code_directory(card_type : str, context_name : str, 
+                                  is_table : bool = False, is_article : bool = False,
+                                  is_str : bool = False, is_pretty : bool = False):
+        code_type = None
+        file_extension = ".html"
+        if is_table:
+                code_type = "Tables"
+        elif is_article:
+                code_type = "Articles"
+        elif is_str:
+                code_type = "Strings"
+                file_extension = ".txt"
+        else:
+                code_type = "Source_Code"
+
+        if code_type is None:
+            raise ValueError("ERROR! code_type is None!")
+
+        if is_pretty:
+            code_type += "/Pretty"
+        
+        return FileHandler.__gen_card_code_directory(card_type, context_name, code_type, file_extension)
+        
 
     
-    def gen_card_source_code_directory(card_type : str, context_name : str):
-        return rf"{global_source_code_output_directory}{card_type}s/{context_name}.html"
+    def gen_card_source_code_directory(card_type : str, context_name : str, is_pretty : bool = False):
+        return FileHandler.__gen_source_code_directory(card_type, context_name, is_pretty=is_pretty)
 
-    def gen_card_article_code_directory(card_type : str, context_name : str):
-        return rf"{global_source_code_output_directory}{card_type}s/Article/{context_name}.html"
-
-    def gen_card_pretty_code_directory(card_type : str, context_name : str) -> str:
-        return rf"{global_source_code_output_directory}{card_type}s/Pretty/{context_name}.html"
+    def gen_card_article_code_directory(card_type : str, context_name : str, is_pretty : bool = False):
+        return FileHandler.__gen_source_code_directory(card_type, context_name, is_article=True, is_pretty=is_pretty)
     
     def gen_card_str_directory(card_type : str, context_name : str):
-        return rf"{global_output_directory}Strings/{card_type}s/{context_name}.txt"
+        return FileHandler.__gen_source_code_directory(card_type, context_name, is_str=True)
 
-    def gen_card_list_source_code_directory(card_type : str, page_number : str):
-        return rf"{global_list_output_directory}{card_type}s/page_{page_number}.html"
-
-    def gen_card_list_pretty_code_directory(card_type : str, page_number : str):
-        return rf"{global_list_output_directory}{card_type}s/Pretty/page_{page_number}.html"
+    def gen_card_list_source_code_directory(card_type : str, page_number : int, is_pretty : bool = False):
+        return FileHandler.__gen_source_code_directory(card_type, f"page_{str(page_number)}", is_table=True, is_pretty=is_pretty)
     
     def _get_global_output_directory():
         return global_output_directory
+    
+
+    
+
+    def move_old_files():
+        for dirpath, dirnames, filenames in os.walk(FileHandler._get_global_output_directory()):
+            for name in filenames:
+                abs_filepath = os.path.join(dirpath, name)
+                
+                # Determine card type
+                card_type = None
+                match dirpath:
+                    case l if "CombatManeuvers" in l:
+                        card_type = "CombatManeuver"
+                    case l if "MagicItems" in l:
+                        card_type = "MagicItem"
+                    case l if "Monsters" in l:
+                        card_type = "Monster"
+                    case l if "Spells" in l:
+                        card_type = "Spell"
+                    case l if "Feats" in l:
+                        card_type = "Feat"
+                
+                # Determine code type (Source_Code or Lists and Pretty or not)
+                code_type = None
+                match dirpath:
+                    case l if "Lists" in l or "Tables" in l:
+                        if "Pretty" in l:
+                            code_type = "Tables/Pretty"
+                        else:
+                            code_type = "Tables"
+                    case l if "Source_Code" in l:
+                        if "Pretty" in l:
+                            code_type = "Source_Code/Pretty"
+                        else:
+                            code_type = "Source_Code"
+
+                # Card type and code type are both known, so we can move the file
+                if card_type and code_type:
+                    new_abs_filepath = FileHandler.__gen_card_code_directory(card_type, name.replace(".html", ""), code_type)
+                    
+                    # If the file doesn't exist yet, move it
+                    if not FileHandler.does_file_exist(new_abs_filepath):
+                        os.makedirs(os.path.dirname(new_abs_filepath), exist_ok=True)
+                        os.rename(abs_filepath, new_abs_filepath)
+                    
+                    # If the file already exists, remove the old file
+                    elif new_abs_filepath != abs_filepath:
+                        print(f"Removed {abs_filepath} because {new_abs_filepath} already exists.")
+                        os.remove(abs_filepath)
+                else:
+                    print(f"ERROR! Could not determine card_type or code_type for {abs_filepath}.")
+                    print(f"card_type: {card_type}")
+                    print(f"code_type: {code_type}")
+
+
+    def rename_old_files():
+        for dirpath, dirnames, filenames in os.walk(FileHandler._get_global_output_directory()):
+            for name in filenames:
+                abs_filepath = os.path.join(dirpath, name)
+
+                new_abs_filepath = abs_filepath   
+
+                # Determine replacement string for renaming the file (if any)
+                replace_str = None 
+                match name:
+                    case s if s.startswith("source_code_of_"):
+                        replace_str = "source_code_of_"
+                    case s if s.startswith("source_code_"):
+                        replace_str = "source_code_"
+                    case s if s.startswith("pretty_code_of_"):
+                        replace_str = "pretty_code_of_"
+                    case s if s.startswith("pretty_code_"):
+                        replace_str = "pretty_code_"
+                    case s if not s.startswith("page_") and "\\Lists\\" in abs_filepath:
+                        new_abs_filepath = os.path.join(dirpath, "page_" + name)
+                    case s if s.startswith("page_") and not ("\\Lists\\" in abs_filepath):
+                        replace_str = "page_"
+
+                # Set new_abs_filepath if replace_str was determined    
+                if replace_str:
+                    new_abs_filepath = os.path.join(dirpath, name.replace(replace_str, ""))
+                
+                # Rename the file if it doesn't exist yet
+                if not FileHandler.does_file_exist(new_abs_filepath):
+                    os.rename(abs_filepath, new_abs_filepath)
+                elif new_abs_filepath != abs_filepath:
+                    os.remove(abs_filepath)
+                    print(f"Removed {abs_filepath} because {new_abs_filepath} already exists.")
