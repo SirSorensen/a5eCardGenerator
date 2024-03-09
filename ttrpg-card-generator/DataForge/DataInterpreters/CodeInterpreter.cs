f9d70e8d941c22972397813621305ae542b396a1
SirSorensen <davidmsoerensen@gmail.com>
2024-03-09 18:33:15 +0100

    Refactor: Initial parts of CodeInterpreter converted

---
+++ ttrpg-card-generator/DataForge/DataInterpreters/CodeInterpreter.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+
+public class CodeInterpreter
+{
+    
+    private HtmlDocument htmlDoc;
+    protected HtmlNode strippedHtmlDocNode;
+
+    public CodeInterpreter(string sourceCode, string strippedTag)
+    {
+        htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(sourceCode);
+        strippedHtmlDocNode = htmlDoc.DocumentNode.SelectSingleNode(strippedTag);
+    }
+
+    // Strip list of strings and return it as a list of strings
+    public static string PrettifyList(HtmlNodeCollection listToPrettify)
+    {
+        var prettifiedList = new List<string>();
+        foreach (var element in listToPrettify)
+        {
+            prettifiedList.Add(GetText(element));
+        }
+        prettifiedList = prettifiedList.Select(item => item.Trim()).ToList();
+        if (prettifiedList.Count == 1)
+        {
+            return prettifiedList[0];
+        }
+        return string.Join(", ", prettifiedList);
+    }
+
+    // Return the text of an element and replace the tags with a whitespace
+    public static string GetText(HtmlNode element)
+    {
+        return Regex.Replace(element.InnerText.Trim(), @"\s{2,}", " ");
+    }
+
+    // Remove all comments from the given html code and return the cleaned html code
+    protected static HtmlNode RemoveComments(HtmlNode htmlNode)
+    {
+        var comments = htmlNode.DescendantsAndSelf().Where(n => n.NodeType == HtmlNodeType.Comment).ToList();
+        foreach (var comment in comments)
+        {
+            comment.Remove();
+        }
+        return htmlNode;
+    }
+
+    // Return a prettified version of the given html code
+    protected static string PrettifyHtml(HtmlNode htmlNode)
+    {
+        htmlNode = RemoveComments(htmlNode);
+
+        string prettyContents = htmlNode.OuterHtml;
+        return prettyContents;
+    }
+
+
+    public string GetPrettySourceHtml()
+    {
+        return PrettifyHtml(htmlDoc.DocumentNode);
+    }
+
+    public string GetStrippedCode()
+    {
+        return strippedHtmlDocNode.OuterHtml;
+    }
+
+    public string GetPrettyStrippedSourceHtml()
+    {
+        return PrettifyHtml(strippedHtmlDocNode);
+    }
+}
